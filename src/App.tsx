2e952fc51cd75172780518f4d30ef12de60b941b
yushe629 <yushe629@users.noreply.github.com>
2020-10-09 16:16:57 +0900

    ブログ参照前

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,26 @@ class CreateTodoFierld extends React.Component {
     );
   }
 }
-class TodoList extends React.Component {}
+
+class TodoList extends React.Component {
+  task1 = {
+    id: 1,
+    task: "初めてのタスク",
+    achieve: true,
+  };
+
+  task2 = {
+    id: 2,
+    task: "2つめのタスク",
+    achieve: true,
+  };
+
+  tasks = [this.task1, this.task2];
+
+  render() {
+    return <div>{this.tasks.map((x) => console.log(x.task))}</div>;
+  }
+}
 
 class App extends React.Component {
   render() {
